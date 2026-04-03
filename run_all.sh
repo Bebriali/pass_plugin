9976b74af937ba6685128bdb481f4778c43177e3
bebriali <ostashkinm@inbox.ru>
2026-04-03 12:20:56 +0300

    modified sh script

--- run_all.sh
+++ run_all.sh
@@ -4,7 +4,7 @@ GREEN='\033[0;32m'
 RED='\033[0;31m'
 NC='\033[0m'
 
-# 1. Проверка входных данных
+# checking input
 if [ -z "$1" ]; then
     echo -e "${RED}Usage: $0 <source_file_name> [args_for_run...]${NC}"
     echo -e "Example: $0 test2.c 5 10"
@@ -12,11 +12,9 @@ if [ -z "$1" ]; then
 fi
 
 SOURCE_FILE=$1
-# Сдвигаем аргументы, чтобы в $@ остались только параметры для запуска программы
 shift
 RUN_ARGS=$@
 
-# Извлекаем имя без расширения для путей (например, test2)
 FILENAME=$(basename -- "$SOURCE_FILE")
 NAME="${FILENAME%.*}"
 
@@ -30,7 +28,6 @@ if [ $? -ne 0 ]; then echo -e "${RED}Failure building plugin!${NC}"; exit 1; fi
 mkdir -p prog
 
 echo -e "${GREEN}>>> Compiling ${SOURCE_FILE} to LLVM IR...${NC}"
-# Используем переменную SOURCE_FILE вместо хардкода
 clang -S -emit-llvm "tests/${SOURCE_FILE}" -o "prog/${NAME}.ll"
 if [ $? -ne 0 ]; then echo -e "${RED}Failure to make IR!${NC}"; exit 1; fi
 
@@ -41,8 +38,6 @@ echo -e "${GREEN}>>> Compiling instrumented binary...${NC}"
 clang++ "prog/${NAME}_inst.ll" scripts/logger.cpp -o run
 
 echo -e "${GREEN}>>> Executing program with args: ${RUN_ARGS}${NC}"
-# Запускаем программу. Она подхватит аргументы командной строки.
-# Если нужен интерактивный ввод (scanf), он просто будет ждать в консоли.
 ./run ${RUN_ARGS}
 
 echo -e "${GREEN}>>> Generating graphs...${NC}"
